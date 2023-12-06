1f351af925655c16391c22f0079fbd51cbf5428a
ManhStrong <tranlemanh2607@gmail.com>
2023-12-06 14:49:02 +0700

    modified vercel

--- app.js
+++ app.js
@@ -1,33 +1,39 @@
 const express = require("express");
 const cors = require("cors");
 import dotenv from "dotenv";
-import util from "util";
-
-require("./index.js");
-
-// const redis = require("redis");
-// const redisUrl = "redis://127.0.0.1:6379";
-// let client = redis.createClient(redisUrl);
-// client.set = util.promisify(client.set);
-
-const initRoutes = require("./routes");
 
 dotenv.config();
+
+const { Sequelize } = require("sequelize");
+const sequelize = new Sequelize(
+  process.env.MYSQL_DB,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+    operatorsAliases: false,
+  }
+);
+
+const connectionDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+connectionDatabase();
+
+const initRoutes = require("./routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.post("/", async (req, res) => {
-//   console.log(req.body, "huhuhu");
-//   const { key, value } = req.body;
-//   const response = await client.set(key, value);
-//   res.json(response);
-// });
 initRoutes(app);
-
-//app.use("/api/users", userRouter);
 
 app.listen(process.env.APP_PORT, () => {
   console.log("Server up and running", process.env.APP_PORT);
