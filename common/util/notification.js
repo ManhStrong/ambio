1f901e4f893af1cf3abc7c5e8758f83ba841de32
ManhStrong <tranlemanh2607@gmail.com>
2023-12-08 16:03:29 +0700

    fix noti

--- common/util/notification.js
+++ common/util/notification.js
@@ -1,11 +1,11 @@
-import FCM from "fcm-node";
-import fetch from "node-fetch";
+import https from "https";
 
 async function sendNotification(deviceTokenCFM) {
   const notification = {
-    title: "Ambio notification414",
+    title: "Ambio notification433",
     text: "Text",
   };
+
   console.log(deviceTokenCFM, 6666);
 
   const fcmToken = [deviceTokenCFM];
@@ -13,23 +13,55 @@ async function sendNotification(deviceTokenCFM) {
     notification: notification,
     registration_ids: fcmToken,
   };
+
   console.log(notificationBody, 78787);
 
   try {
     console.log(1234567889);
-    const response = await fetch("https://fcm.googleapis.com/fcm/send", {
-      method: "POST",
-      headers: {
-        Authorization:
-          "key=" +
-          "AAAA5kqLEa8:APA91bF5ycQHHy1U7yP_tskNjvWMg-zGqR-6R86S_8Y95uN8tEL99IZIa8jaLAbUdkNGUQLBeaFoh4BdETH9HU0pkJeg3QiewiDRK6P7lrbARAZ0HdxYRlgC38k5DSfe4J3d2y5vMT3Q",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(notificationBody),
+
+    // Tạo promise để xử lý yêu cầu HTTPS
+    const response = await new Promise((resolve, reject) => {
+      const req = https.request(
+        "https://fcm.googleapis.com/fcm/send",
+        {
+          method: "POST",
+          headers: {
+            Authorization:
+              "key=" +
+              "AAAA5kqLEa8:APA91bF5ycQHHy1U7yP_tskNjvWMg-zGqR-6R86S_8Y95uN8tEL99IZIa8jaLAbUdkNGUQLBeaFoh4BdETH9HU0pkJeg3QiewiDRK6P7lrbARAZ0HdxYRlgC38k5DSfe4J3d2y5vMT3Q",
+            "Content-Type": "application/json",
+          },
+        },
+        (res) => {
+          let data = "";
+
+          res.on("data", (chunk) => {
+            data += chunk;
+          });
+
+          res.on("end", () => {
+            resolve({ status: res.statusCode, data });
+          });
+        }
+      );
+
+      req.on("error", (err) => {
+        reject(err);
+      });
+
+      req.write(JSON.stringify(notificationBody));
+      req.end();
+
+      // Thiết lập thời gian timeout cho yêu cầu
+      req.setTimeout(10000, () => {
+        req.abort(); // Hủy yêu cầu khi timeout
+        reject(new Error("Request Timeout"));
+      });
     });
+
     console.log(response, 88888);
 
-    if (response.ok) {
+    if (response.status === 200) {
       console.log("Thành công");
     } else {
       console.log("Lỗi trong quá trình gửi thông báo");
