93e40a35807675202343c05ed23a70cbf3eac59a
ManhStrong <tranlemanh2607@gmail.com>
2023-12-08 10:45:49 +0700

    sua axios

--- common/util/notification.js
+++ common/util/notification.js
@@ -14,24 +14,21 @@ async function sendNotification(deviceTokenCFM) {
   };
 
   try {
-    const response = await axios.post(
-      "https://fcm.googleapis.com/fcm/send",
-      notificationBody,
-      {
+    await axios
+      .post("https://fcm.googleapis.com/fcm/send", notificationBody, {
         headers: {
           Authorization:
             "key=" +
             "AAAA5kqLEa8:APA91bF5ycQHHy1U7yP_tskNjvWMg-zGqR-6R86S_8Y95uN8tEL99IZIa8jaLAbUdkNGUQLBeaFoh4BdETH9HU0pkJeg3QiewiDRK6P7lrbARAZ0HdxYRlgC38k5DSfe4J3d2y5vMT3Q",
           "Content-Type": "application/json",
         },
-      }
-    );
-
-    if (response.status === 200) {
-      console.log("Thành công");
-    } else {
-      console.log("Lỗi trong quá trình gửi thông báo");
-    }
+      })
+      .then((response) => {
+        console.log("Thanh cong");
+      })
+      .catch((err) => {
+        console.log("that bai");
+      });
   } catch (err) {
     console.error(err);
   }
