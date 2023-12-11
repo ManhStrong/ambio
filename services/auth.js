c18d5254046a684a6d308862f6e324f36bb8846b
ManhStrong <tranlemanh2607@gmail.com>
2023-12-11 08:58:56 +0700

    sua loi

--- services/auth.js
+++ services/auth.js
@@ -158,7 +158,40 @@ export const loginService = async ({
         }
       );
       if (userInfo.isOriginDevice === false) {
-        sendNotification(user.deviceTokenCFM);
+        const notification = {
+          title: "Ambio notification414",
+          text: "Text",
+        };
+
+        const fcmToken = [user.deviceTokenCFM];
+        const notificationBody = {
+          notification: notification,
+          registration_ids: fcmToken,
+        };
+        console.log(notificationBody, 78787);
+
+        try {
+          console.log(1234567889);
+          const response = await fetch("https://fcm.googleapis.com/fcm/send", {
+            method: "POST",
+            headers: {
+              Authorization:
+                "key=" +
+                "AAAA5kqLEa8:APA91bF5ycQHHy1U7yP_tskNjvWMg-zGqR-6R86S_8Y95uN8tEL99IZIa8jaLAbUdkNGUQLBeaFoh4BdETH9HU0pkJeg3QiewiDRK6P7lrbARAZ0HdxYRlgC38k5DSfe4J3d2y5vMT3Q",
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(notificationBody),
+          });
+          console.log(response, 88888);
+
+          if (response.ok) {
+            console.log("Thành công");
+          } else {
+            console.log("Lỗi trong quá trình gửi thông báo");
+          }
+        } catch (err) {
+          console.error(err, 161616);
+        }
       }
       return {
         accessToken: userInfo.token,
@@ -173,7 +206,41 @@ export const loginService = async ({
         isOriginDevice: false,
         expirationTime: moment().add(7, "days"),
       });
-      sendNotification(user.deviceTokenCFM);
+      // sendNotification(user.deviceTokenCFM);
+      const notification = {
+        title: "Ambio notification414",
+        text: "Text",
+      };
+
+      const fcmToken = [user.deviceTokenCFM];
+      const notificationBody = {
+        notification: notification,
+        registration_ids: fcmToken,
+      };
+      console.log(notificationBody, 78787);
+
+      try {
+        console.log(1234567889);
+        const response = await fetch("https://fcm.googleapis.com/fcm/send", {
+          method: "POST",
+          headers: {
+            Authorization:
+              "key=" +
+              "AAAA5kqLEa8:APA91bF5ycQHHy1U7yP_tskNjvWMg-zGqR-6R86S_8Y95uN8tEL99IZIa8jaLAbUdkNGUQLBeaFoh4BdETH9HU0pkJeg3QiewiDRK6P7lrbARAZ0HdxYRlgC38k5DSfe4J3d2y5vMT3Q",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(notificationBody),
+        });
+        console.log(response, 88888);
+
+        if (response.ok) {
+          console.log("Thành công");
+        } else {
+          console.log("Lỗi trong quá trình gửi thông báo");
+        }
+      } catch (err) {
+        console.error(err, 161616);
+      }
       return {
         accessToken: token,
       };
